2f82b8025e8a648245da50968e2ae4b6174bbe5d
tyler <hisday@gmail.com>
2021-02-11 00:14:43 +0900

    Add propTypes

--- src/App.js
+++ src/App.js
@@ -1,57 +1,66 @@
-function Potato({ name, picture }) {
+import PropTypes from "prop-types"
+
+function Food({ name, rating, picture }) {
   // console.log(name)
   return (
       <div>
           <h3>I love {name}</h3>
+          <h4>rating: {rating}/5.0 </h4>
           <img src={picture} alt={name + 'keke'} />
       </div>
   )
+}
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
 }
 
 const foodILike = [
   {
     id: 1,
     name: "Kimchi",
+    rating: 5.0,
     image:
       "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
   },
   {
     id: 2,
     name: "Samgyeopsal",
+    rating: 3.5,
     image:
       "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
   },
   {
     id: 3,
     name: "Bibimbap",
+    rating: 4.8,
     image:
       "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
   },
   {
     id: 4,
     name: "Doncasu",
+    rating: 3.2,
     image:
       "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
   },
   {
     id: 5,
     name: "Kimbap",
+    rating: 3.8,
     image:
       "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
   }
 ];
 
-
-function renderFood(dish) {
-  // console.log(dish)
-  return <Potato key={dish.id} name={dish.name} picture={dish.image} />
-}
-
 function App() {
   return (
     <div className="App">
       <h1>Hello tw World</h1>
-      {foodILike.map(renderFood)}
+      {foodILike.map(dish => 
+        <Food key={dish.id} rating={dish.rating} name={dish.name} picture={dish.image} />)}
     </div>
   );
 }
