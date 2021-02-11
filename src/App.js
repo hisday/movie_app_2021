74e733d2a983ed81e036ea8a1540b9d33a5ded6c
tyler <hisday@gmail.com>
2021-02-11 11:53:38 +0900

    3.2 Component life cycle

--- src/App.js
+++ src/App.js
@@ -1,68 +1,40 @@
-import PropTypes from "prop-types"
+import React from "react"
 
-function Food({ name, rating, picture }) {
-  // console.log(name)
-  return (
-      <div>
-          <h3>I love {name}</h3>
-          <h4>rating: {rating}/5.0 </h4>
-          <img src={picture} alt={name + 'keke'} />
-      </div>
-  )
-}
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired
-}
-
-const foodILike = [
-  {
-    id: 1,
-    name: "Kimchi",
-    rating: 5.0,
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
-  },
-  {
-    id: 2,
-    name: "Samgyeopsal",
-    rating: 3.5,
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
-  },
-  {
-    id: 3,
-    name: "Bibimbap",
-    rating: 4.8,
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
-  },
-  {
-    id: 4,
-    name: "Doncasu",
-    rating: 3.2,
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
-  },
-  {
-    id: 5,
-    name: "Kimbap",
-    rating: 3.8,
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    console.log("constructor")
   }
-];
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello tw World</h1>
-      {foodILike.map(dish => 
-        <Food key={dish.id} rating={dish.rating} name={dish.name} picture={dish.image} />)}
-    </div>
-  );
+  componentDidMount() {
+    console.log("componentDidMount")
+  }
+  componentDidUpdate() {
+    console.log("componentDidUpdate")
+  }
+  componentWillUnmount() {
+    console.log("componentWillUnmount")
+  }
+  state = {
+    count: 0
+  }
+  add = () => {
+    console.log("add")
+    this.setState({count: this.state.count + 1})
+  }
+  minus = () => {
+    console.log('minus')
+    this.setState({count: this.state.count - 1})
+  }
+  render() {
+    console.log("render")
+    return (
+      <div>
+        <h1>number is {this.state.count}</h1>
+        <button onClick={this.add}>add</button>
+        <button onClick={this.minus}>minus</button>
+      </div>
+    )
+  }
 }
 
 export default App;
